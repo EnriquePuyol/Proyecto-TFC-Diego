41b5c5f3956ddaed1e977c4713d73c6e5486dca4
Enrique Puyol <kpuyol94@hotmail.com>
2019-06-13 12:51:23 +0200

    Initial commit

---
+++ Assets/Scipts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 10f;
+    private  Vector3 offset;
+
+    void Start()
+    {
+        offset = target.position - transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.RotateAround(target.position, Vector3.up, 10 * Time.deltaTime);
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position - offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scipts/Movimiento.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Movimiento : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                transform.LookAt(hit.point);
+
+                agent.SetDestination(hit.point);
+                Debug.Log("MOVEMENT");
+            }
+        }
+    }
+}
