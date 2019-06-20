c1061593fcfca5d76c1f06649420762c276dc076
DIEGUS01 <51441348+DIEGUS01@users.noreply.github.com>
2019-06-20 11:12:55 +0200

    nueva escena menu principal botones pantalla de carga

---
+++ Assets/Scipts/MenuInicial.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+
+    public Image LoadingImage;
+
+    public Text Percentage;
+
+
+    public void Play(int scene)
+    {
+        LoadingImage.gameObject.SetActive(true);
+        StartCoroutine(LoadLevel(scene));
+    }
+
+
+    public void Options()
+    {
+
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadLevel(int scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Percentage.text = progress * 100f + "%";
+            yield return null;
+        }     
+         
+    }
+
+
+}
