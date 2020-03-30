58605ef92a422ba9723b6383c585a10ead23bb92
DIEGUS01 <51441348+DIEGUS01@users.noreply.github.com>
2020-03-30 12:05:25 +0200

    game object de misiones

---
+++ Assets/Scripts/MisionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject MisionCasaPrincipal;
+    public GameObject MisionCasaAbuela;
+    public GameObject MisionColegio;
+    public GameObject MisionBar;
+    public GameObject MisionComisaria;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
