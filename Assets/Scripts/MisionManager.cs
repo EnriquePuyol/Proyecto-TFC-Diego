cc0426bf434e6072bdc66f2ebf1d769a38bc23bb
DIEGUS01 <51441348+DIEGUS01@users.noreply.github.com>
2020-04-04 11:41:20 +0200

    introduccion de los cruces y los tags de las misiones

--- Assets/Scripts/MisionManager.cs
+++ Assets/Scripts/MisionManager.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -11,15 +10,45 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject MisionBar;
     public GameObject MisionComisaria;
 
+    private string MisionCasaPrincipalText = "Llega a tu casa antes de que se acabe el tiempo";
+    private string MisionCasaAbuelaText = "Llega a casa de la abuela antes de que se acabe el tiempo";
+    private string MisionColegioText = "Llega al colegio antes de que se acabe el tiempo";
+    private string MisionBarText = "Llega al bar antes de que se acabe el tiempo";
+    private string MisionComisariaText = "Llega a la comisaria antes de que se acabe el tiempo";
+
+    private int MisionActual;
+    private Text MisionText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MisionActual = Random.Range(1,6);
+        switch (MisionActual)
+        {
+            case 1:
+                MisionText.text = MisionCasaPrincipalText;
+                break;
+            case 2:
+                MisionText.text = MisionCasaAbuelaText;
+                break;
+            case 3:
+                MisionText.text = MisionColegioText;
+                break;
+            case 4: MisionText.text = MisionBarText;
+                break;
+            case 5:
+                MisionText.text = MisionComisariaText;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         
     }
