59499f6bfd4e37806dc7f04e45a34f84b459a6df
Enrique Puyol <kpuyol94@hotmail.com>
2020-04-20 10:43:40 +0200

    Map finished

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    Camera MainCamera;
+
+    [SerializeField]
+    Camera MapCamera;
+
+    [SerializeField]
+    GameObject canvas;
+
+    [SerializeField]
+    Movimiento player;
+
+    bool mapEnable = false;
+
+    void Start()
+    {
+        MainCamera.enabled = true;
+        MapCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.M))
+        {
+            if(mapEnable == false)
+            {
+                mapEnable = true;
+
+                MainCamera.enabled = false;
+                MapCamera.enabled = true;
+
+                canvas.SetActive(false);
+                player.pausado = true;
+            }
+            else
+            {
+                mapEnable = false;
+
+                MainCamera.enabled = true;
+                MapCamera.enabled = false;
+
+                canvas.SetActive(true);
+                player.pausado = false;
+            }
+        }
+    }
+}
