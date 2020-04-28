5e2c36cfec0cc2f3e10899a55bbb3739f73e93e3
DIEGUS01 <51441348+DIEGUS01@users.noreply.github.com>
2020-04-28 19:05:23 +0200

    28 04 20

---
+++ Assets/Scripts/IAWalk.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAWalk : MonoBehaviour
+{
+    public float Speed = 4.0f;
+    public Vector3[] posiciones;
+    int indexActual = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = posiciones[0];
+        transform.LookAt(posiciones[indexActual]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, posiciones[indexActual], Speed * Time.deltaTime);
+
+        if (transform.position == posiciones[indexActual])
+        {
+            if(indexActual == posiciones.Length - 1)
+            {
+                indexActual = 0;
+            }
+            else
+            {
+                indexActual++;
+            }
+            
+            transform.LookAt(posiciones[indexActual]);
+        }
+    }
+}
